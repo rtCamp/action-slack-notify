f2258a31011ab5005fff1461f01dc96a8dddf94f
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-03-26 14:21:21 +0530

    Fix getting env variables in fallback message
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 		Channel:  os.Getenv(EnvSlackChannel),
 		Attachments: []Attachment{
 			{
-				Fallback: envOr(EnvSlackMessage, "GITHUB_ACTION=${GITHUB_ACTION} \n GITHUB_ACTOR=${GITHUB_ACTOR} \n GITHUB_EVENT_NAME=${GITHUB_EVENT_NAME} \n GITHUB_REF=${GITHUB_REF} \n GITHUB_REPOSITORY=${GITHUB_REPOSITORY} \n GITHUB_WORKFLOW=${GITHUB_WORKFLOW}"),
+				Fallback: envOr(EnvSlackMessage, "GITHUB_ACTION=" + os.Getenv("GITHUB_ACTION") + " \n GITHUB_ACTOR=" + os.Getenv("GITHUB_ACTOR") + " \n GITHUB_EVENT_NAME=" + os.Getenv("GITHUB_EVENT_NAME") + " \n GITHUB_REF=" + os.Getenv("GITHUB_REF") + " \n GITHUB_REPOSITORY=" + os.Getenv("GITHUB_REPOSITORY") + " \n GITHUB_WORKFLOW=" + os.Getenv("GITHUB_WORKFLOW"),
 				Color:      envOr(EnvSlackColor, "good"),
 				AuthorName: envOr(EnvGithubActor, ""),
 				AuthorLink: "http://github.com/" + os.Getenv(EnvGithubActor),
