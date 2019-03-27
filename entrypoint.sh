306bb4c77cf768e5635695c35725133ce7f89db2
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-03-27 17:07:19 +0530

    Add missing secret error handling
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# Check required env variables
+flag=0
+if [[ -z "$SLACK_WEBHOOK" ]]; then
+    flag=1
+    missing_secret="SLACK_WEBHOOK"
+    if [[ -n "$VAULT_ADDR" ]] && [[ -n "$VAULT_TOKEN" ]]; then
+        flag=0
+    fi
+    if [[ -n "$VAULT_ADDR" ]] || [[ -n "$VAULT_TOKEN" ]]; then
+        missing_secret="VAULT_ADDR and/or VAULT_TOKEN"
+    fi
+fi
+
+if [[ "$flag" -eq 1 ]]; then
+    printf "[\e[0;31mERROR\e[0m] Secret \`$missing_secret\` is missing. Please add it to this action for proper execution.\nRefer https://github.com/rtCamp/action-slack-notify for more information.\n"
+    exit 1
+fi
+
 export GITHUB_BRANCH=${GITHUB_REF##*heads/}
 export SLACK_ICON=${SLACK_ICON:-"https://avatars0.githubusercontent.com/u/43742164"}
 export SLACK_USERNAME=${SLACK_USERNAME:-"rtBot"}
